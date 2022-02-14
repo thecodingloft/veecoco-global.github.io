b1677dfa9b6a60002e8bf658017ff2460015601e
thecodingloft <99214127+thecodingloft@users.noreply.github.com>
2022-02-14 15:31:51 +0100

    Update custom-script.js

--- custom-script.js
+++ custom-script.js
@@ -2,4 +2,4 @@ console.log("js script working...)
 
 const testClass = document.querySelector('.test_class')
 
-testClass.innerText = "Number of courses: 20"
+testClass.innerText = "Hello there again"
